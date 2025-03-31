3251761bbf3083367c6b8677197e4bb19afe43b4
Kailas Walldoddi <kailaswalldoddi@Kailass-MacBook-Air.local>
2025-03-31 14:06:41 +0400

    docker fixes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from app.analysis import analyze_book
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
@@ -11,6 +13,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+def read_index():
+    return FileResponse("static/index.html")
 
 @app.get("/analyze/{book_id}")
 async def analyze(book_id: int):
